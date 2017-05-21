081a456b114735c5d72adf6267d19cbb943ac397
Andy Vo <andy@voandy.com>
2017-05-21 22:05:17 +1000

    made a little progress on rotor class

--- enigma_machine.py
+++ enigma_machine.py
@@ -1,8 +1,10 @@
+from string import ascii_lowercase as lower
 import csv
 
 index = []
 rotor_wiring = []
 reflector_wiring = []
+letter_dict = {letter:i for i, letter in enumerate(lower, 1)}
 
 # reads in rotor and reflector wiring
 with open('rotor_wiring.csv') as wiring:
@@ -12,8 +14,7 @@ with open('rotor_wiring.csv') as wiring:
 	index = next(data)
 
 	next(data)
-	for i in range(5):
-		rotor_wiring.append(next(data))
+	rotor_wiring = [next(data) for i in range(5)]
 
 	next(data)
 	reflector_wiring = next(data)
@@ -24,11 +25,14 @@ class rotor:
 
 	def __init__(self, rotor_no, start_pos):
 		self.rotor_start = rotor_wiring[rotor_no]
-		print(self.rotor_start)
-		self.rotor_curr = rotor_start[start_pos:] + rotor_start[:start_pos]
+		self.rotor_curr = self.rotor_start[start_pos:] + \
+			self.rotor_start[:start_pos]
 
 	def encrypt(self, char):
 		return self
 
 	def encrypt_reverse(self, char):
 		pass
+
+def convert_char(char):
+	pass
--- test.py
+++ test.py
@@ -3,3 +3,4 @@ from enigma_machine import *
 rotor_right = rotor(0, 3)
 
 print(rotor_right.rotor_curr)
+
