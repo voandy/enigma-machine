36dc03c741669367207e951852b14838976c8680
Andy Vo <andy@voandy.com>
2017-05-21 23:14:37 +1000

    more progress made on rotor class

--- enigma_machine.py
+++ enigma_machine.py
@@ -1,17 +1,17 @@
-from string import ascii_lowercase as lower
+from string import ascii_uppercase as uper
 import csv
 
-index = []
+# declare and import setting for the rotors and reflector
+rotor_index = []
 rotor_wiring = []
 reflector_wiring = []
-letter_dict = {letter:i for i, letter in enumerate(lower, 1)}
+letter_dict = {letter:i for i, letter in enumerate(uper)}
 
-# reads in rotor and reflector wiring
-with open('rotor_wiring.csv') as wiring:
+with open('wiring.csv') as wiring:
 	data = csv.reader(wiring)
 	
 	next(data) # skips header
-	index = next(data)
+	rotor_index = next(data)
 
 	next(data)
 	rotor_wiring = [next(data) for i in range(5)]
@@ -19,20 +19,37 @@ with open('rotor_wiring.csv') as wiring:
 	next(data)
 	reflector_wiring = next(data)
 
+class plug_board:
+	pass
+
 class rotor:
 	rotor_start = []
 	rotor_curr = []
+	index_curr = []
 
 	def __init__(self, rotor_no, start_pos):
 		self.rotor_start = rotor_wiring[rotor_no]
 		self.rotor_curr = self.rotor_start[start_pos:] + \
 			self.rotor_start[:start_pos]
+		self.index_curr = rotor_index[start_pos:] + rotor_index[:start_pos]
 
-	def encrypt(self, char):
-		return self
+	def encrypt(self, num):
+		return self.rotor_curr.index(self.index_curr[num])
 
 	def encrypt_reverse(self, char):
 		pass
 
+	def rotate(self):
+		self.rotor_curr = self.rotor_curr[1:] + self.rotor_curr[:1]
+		self.index_curr = self.index_curr[1:] + self.index_curr[:1]
+
+class reflector:
+	pass
+
 def convert_char(char):
+	return letter_dict[char.upper()]
+
+
+
+class machine:
 	pass
--- test.py
+++ test.py
@@ -3,4 +3,15 @@ from enigma_machine import *
 rotor_right = rotor(0, 3)
 
 print(rotor_right.rotor_curr)
+print(rotor_right.index_curr)
 
+rotor_right.rotate()
+
+print("\n")
+
+print(rotor_right.rotor_curr)
+print(rotor_right.index_curr)
+
+print("\n")
+
+print(rotor_right.encrypt(0))
